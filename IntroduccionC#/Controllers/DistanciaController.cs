5c2d06f0560c006457aa9c7b861ac069315809ad
IDGS-904-23001531 <stefaniapro5@gmail.com>
2026-05-14 13:44:00 -0600

    Distancia 13/05/2026

---
+++ IntroduccionC#/Controllers/DistanciaController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.UI.WebControls;
+using IntroduccionC_.Models;
+
+namespace IntroduccionC_.Controllers
+{
+    public class DistanciaController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Calcular(Distancia dis)
+        {
+            dis.CalcularDistancia();
+            return View(dis);
+        }
+
+    }
+}
---
+++ IntroduccionC#/Models/Distancia.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace IntroduccionC_.Models
+{
+    public class Distancia
+    {
+        public double x1 { get; set; }
+        public double y1 { get; set; }
+        public double x2 { get; set; }
+        public double y2 { get; set; }
+        public double res { get; set; }
+
+        public void CalcularDistancia()
+        {
+            this.res = Math.Sqrt(
+                Math.Pow(this.x2 - this.x1, 2) +
+                Math.Pow(this.y2 - this.y1, 2));
+        }
+
+    }
+}
